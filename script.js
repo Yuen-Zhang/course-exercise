c5f9d20409e9564e80732325b5a28c3244b6a568
BryanHerrera94 <130613587+BryanHerrera94@users.noreply.github.com>
2023-06-14 11:40:48 -0700

    first exercise

---
+++ script.js
@@ -0,0 +1,21 @@
+fetch('http://localhost:8081/api/courses')
+      .then(response => response.json())
+      .then(data => {
+        const table = document.getElementById('course-table');
+
+        data.forEach(course => {
+          const { id, dept, courseNum, courseName, instructor, startDate, numDays } = course;
+          const row = document.createElement('tr');
+          row.innerHTML = `
+            <td>${id}</td>
+            <td>${dept}</td>
+            <td>${courseNum}</td>
+            <td>${courseName}</td>
+            <td>${instructor}</td>
+            <td>${startDate}</td>
+            <td>${numDays}</td>
+          `;
+          table.appendChild(row);
+        });
+      })
+     
