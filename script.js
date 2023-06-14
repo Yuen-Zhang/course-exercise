d0b0cdc5eb512a84f0155ed1580aade802c09df9
BryanHerrera94 <130613587+BryanHerrera94@users.noreply.github.com>
2023-06-14 11:58:31 -0700

    fixed requirements
    changed the table

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 fetch('http://localhost:8081/api/courses')
-      .then(response => response.json())
-      .then(data => {
-        const table = document.getElementById('course-table');
+  .then(response => response.json())
+  .then(data => {
+    const table = document.getElementById('course-table');
 
-        data.forEach(course => {
-          const { id, dept, courseNum, courseName, instructor, startDate, numDays } = course;
-          const row = document.createElement('tr');
-          row.innerHTML = `
-            <td>${id}</td>
+    data.forEach(course => {
+      const { id, dept, courseNum, courseName, instructor, startDate, numDays } = course;
+      const row = document.createElement('tr');
+      row.innerHTML = `
+            
             <td>${dept}</td>
             <td>${courseNum}</td>
             <td>${courseName}</td>
-            <td>${instructor}</td>
-            <td>${startDate}</td>
-            <td>${numDays}</td>
+            
+            
+           
           `;
-          table.appendChild(row);
-        });
-      })
-     
+      table.appendChild(row);
+    });
+  })
