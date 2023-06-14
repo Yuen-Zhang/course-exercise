bca6a009c4f40e62f74764a08ee583fe9afecbb1
BryanHerrera94 <130613587+BryanHerrera94@users.noreply.github.com>
2023-06-14 13:57:58 -0700

    details page working

--- script.js
+++ script.js
@@ -7,14 +7,28 @@ fetch('http://localhost:8081/api/courses')
       const { id, dept, courseNum, courseName, instructor, startDate, numDays } = course;
       const row = document.createElement('tr');
       row.innerHTML = `
-            
-            <td>${dept}</td>
-            <td>${courseNum}</td>
-            <td>${courseName}</td>
-            
-            
-           
-          `;
+        <td>${dept}</td>
+        <td>${courseNum}</td>
+        <td><a href="#" data-course-id="${id}">${courseName}</a></td>
+      `;
       table.appendChild(row);
+
+      // Add click event listener to the course link
+      const courseLink = row.querySelector('a');
+      courseLink.addEventListener('click', event => {
+        event.preventDefault();
+
+        // Get the course ID from the data attribute
+        const courseId = courseLink.dataset.courseId;
+
+        // Find the selected course
+        const selectedCourse = data.find(course => course.id === courseId);
+
+        
+        localStorage.setItem('selectedCourse', JSON.stringify(selectedCourse));
+
+        
+        window.location.href = 'details.html';
+      });
     });
-  })
+  });
